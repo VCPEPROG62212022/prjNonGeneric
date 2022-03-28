e9f235bdebed511c99027773483aa4e5a4287dfa
Reece Wanvig <rwanvig@iie.ac.za>
2022-03-28 15:43:23 +0200

    Add project files.

---
+++ prjNonGeneric/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace prjNonGeneric
+{
+    partial class frmNonGeneric
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.lstDictionary = new System.Windows.Forms.ListBox();
+            this.lstHashset = new System.Windows.Forms.ListBox();
+            this.lstQueue = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(47, 25);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(75, 23);
+            this.btnPrint.TabIndex = 0;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // lstDictionary
+            // 
+            this.lstDictionary.FormattingEnabled = true;
+            this.lstDictionary.ItemHeight = 15;
+            this.lstDictionary.Location = new System.Drawing.Point(13, 57);
+            this.lstDictionary.Name = "lstDictionary";
+            this.lstDictionary.Size = new System.Drawing.Size(164, 124);
+            this.lstDictionary.TabIndex = 1;
+            // 
+            // lstHashset
+            // 
+            this.lstHashset.FormattingEnabled = true;
+            this.lstHashset.ItemHeight = 15;
+            this.lstHashset.Location = new System.Drawing.Point(183, 57);
+            this.lstHashset.Name = "lstHashset";
+            this.lstHashset.Size = new System.Drawing.Size(164, 124);
+            this.lstHashset.TabIndex = 2;
+            // 
+            // lstQueue
+            // 
+            this.lstQueue.FormattingEnabled = true;
+            this.lstQueue.ItemHeight = 15;
+            this.lstQueue.Location = new System.Drawing.Point(353, 57);
+            this.lstQueue.Name = "lstQueue";
+            this.lstQueue.Size = new System.Drawing.Size(164, 124);
+            this.lstQueue.TabIndex = 3;
+            // 
+            // frmNonGeneric
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 201);
+            this.Controls.Add(this.lstQueue);
+            this.Controls.Add(this.lstHashset);
+            this.Controls.Add(this.lstDictionary);
+            this.Controls.Add(this.btnPrint);
+            this.Name = "frmNonGeneric";
+            this.Text = "Non Generic";
+            this.Load += new System.EventHandler(this.frmNonGeneric_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.ListBox lstDictionary;
+        private System.Windows.Forms.ListBox lstHashset;
+        private System.Windows.Forms.ListBox lstQueue;
+    }
+}
+
---
+++ prjNonGeneric/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjNonGeneric
+{
+    public partial class frmNonGeneric : Form
+    {
+        public frmNonGeneric()
+        {
+            InitializeComponent();
+
+            btnPrint.Click +=
+
+                delegate (object sender, EventArgs e)
+                {
+                    printDictionary();
+                    printHashset();
+                    printQueue();
+
+                };
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void printDictionary()
+        {
+
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+            dictionary.Add("DIS1", 40);
+            dictionary.Add("DIS2", 25);
+            dictionary.Add("DIS3", 21);
+
+            String strCourse = "DIS2";
+            if (dictionary.ContainsKey(strCourse))
+            {
+                int Value = dictionary[strCourse];
+                this.lstDictionary.Items.Add(strCourse + " has " + Value + " students.");
+            }
+
+            this.lstDictionary.Items.Add("Dictionary has " + dictionary.Count + " courses ");
+        }
+        private void printHashset()
+        {
+            string[] array1 =
+            {
+                "C#",
+                "Java",
+                "Java",
+                "PHP",
+                "HTML",
+                "VB.net"
+            };
+
+            var hash = new HashSet<string>(array1);
+            string[] array2 = hash.ToArray();
+            this.lstHashset.Items.Add(string.Join(",", array2));
+        }
+
+        private void printQueue()
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(5);
+            queue.Enqueue(10);
+            queue.Enqueue(15);
+            queue.Enqueue(20);
+
+
+            this.lstQueue.Items.Add("Results from the queue (FIFO)");
+            foreach (var item in queue)
+            {
+                this.lstQueue.Items.Add(item);
+            }
+
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(5);
+            stack.Push(10);
+            stack.Push(15);
+            stack.Push(20);
+
+            this.lstQueue.Items.Add("Results from the stack (LIFO)");
+            foreach (var item in stack)
+            {
+                this.lstQueue.Items.Add(item);
+            }
+
+        }
+
+        private void frmNonGeneric_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
